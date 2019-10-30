2f0336b919fc8f0eeeac652c0c140a5feaf7ec47
Can Gencer <cgencer@gmail.com>
2019-10-30 08:49:05 +0100

    cleanup query

--- src/main/java/Query1.java
+++ src/main/java/Query1.java
@@ -3,7 +3,6 @@ import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.accumulator.MutableReference;
 import com.hazelcast.jet.aggregate.AggregateOperation;
 import com.hazelcast.jet.aggregate.AggregateOperation1;
-import com.hazelcast.jet.aggregate.AggregateOperations;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.config.ProcessingGuarantee;
 import com.hazelcast.jet.datamodel.Tuple3;
@@ -18,13 +17,10 @@ import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map.Entry;
 import java.util.Properties;
 
 import static com.hazelcast.jet.aggregate.AggregateOperations.allOf;
-import static com.hazelcast.jet.aggregate.AggregateOperations.averagingLong;
 import static com.hazelcast.jet.aggregate.AggregateOperations.counting;
 import static com.hazelcast.jet.aggregate.AggregateOperations.summingLong;
 
@@ -76,25 +72,9 @@ public class Query1 {
         aggregated
                 .drainTo(Sinks.map("query1_Results"));
 
-
         // write results to Kafka topic
 //        aggregated
 //                .drainTo(KafkaSinks.kafka(kafkaSinkProps(servers), "query1_Results"));
-
-
-        // add detail rows to IMap
-//        source
-//                .drainTo(Sinks.<Trade, String, List<String>>mapWithUpdating(
-//                        "query1_Trades", trade -> trade.getSymbol(),
-//                        (list, trade) -> {
-//                            if (list == null) {
-//                                list = new ArrayList<>();
-//                            }
-//                            list.add(trade.getTradeId());
-//                            return list;
-//                        }));
-
-        // add detail rows to multi map as (symbol, trade)
         return p;
     }
 
