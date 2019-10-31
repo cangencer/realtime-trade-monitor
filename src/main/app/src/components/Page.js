ca21a9ad8b138174a74fc8c93e854c5e078a9dcc
Tomas Pustelnik <10606260+Pustelto@users.noreply.github.com>
2019-10-31 10:55:50 +0100

    feat: common styles

--- src/main/app/src/components/Page.js
+++ src/main/app/src/components/Page.js
@@ -3,9 +3,12 @@ import React from 'react'
 const Page = ({header, children}) =>
     <div className="App">
         <header className="App-header">
-            {header && <h1>{header}</h1>}
-            {children}
+            <h1 className="App-title">Trade Monitor Dashboard</h1>
+            <span className="App-subtitle">powered by <span class="sr-only">hazelcast</span>&nbsp;<img className="hazelcastLogo" src="/images/hazelcast_logo.png" srcset="" alt=""/></span>
         </header>
+        <main>
+            {children}
+        </main>
     </div>
 
 export default Page
--- src/main/app/src/components/not-found/not-found.js
+++ src/main/app/src/components/not-found/not-found.js
@@ -6,6 +6,7 @@ import Page     from '../Page'
 
 const NotFound = () => (
     <Page header="Not Found">
+        <h2>Page not found</h2>
         <span><Link className="App-link" to="/">Home</Link></span>
     </Page>
 )
