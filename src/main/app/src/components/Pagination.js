81a83cafd204a3f1010a4b0907d4b2e1b4c0a66c
Tomas Pustelnik <10606260+Pustelto@users.noreply.github.com>
2019-11-01 10:40:49 +0100

    feat: expander, noData styles, small tweaks

--- src/main/app/src/components/Pagination.js
+++ src/main/app/src/components/Pagination.js
@@ -1,5 +1,5 @@
-import React, {Component, Fragment} from 'react';
-import './Pagination.css';
+import React, { Component, Fragment } from "react";
+import "./Pagination.css";
 
 /*
  * Many parts are copied from defualt pagination component in order to provide expected behaivior.
@@ -7,8 +7,8 @@ import './Pagination.css';
  */
 class Pagination extends Component {
   static defaultProps = {
-    extraData: undefined,
-  }
+    extraData: undefined
+  };
 
   constructor(props) {
     super(props);
@@ -21,55 +21,56 @@ class Pagination extends Component {
     this.onPageSizeChange = this.onPageSizeChange.bind(this);
 
     this.state = {
-      page: props.page,
-    }
+      page: props.page
+    };
   }
 
-  componentDidUpdate (prevProps, prevState) {
-    console.log(prevProps.page, this.props.page, prevState.page, this.state.page);
-
-    if (prevProps.page !== this.props.page && prevState.page !== this.state.page) {
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.page !== this.props.page &&
+      prevState.page !== this.state.page
+    ) {
       // this is probably safe because we only update when old/new state.page are different
       // eslint-disable-next-line react/no-did-update-set-state
       this.setState({
-        page: this.props.page,
-      })
+        page: this.props.page
+      });
     }
   }
 
-  getSafePage (page) {
+  getSafePage(page) {
     if (Number.isNaN(page)) {
-      page = this.props.page
+      page = this.props.page;
     }
-    return Math.min(Math.max(page, 0), this.props.pages - 1)
+    return Math.min(Math.max(page, 0), this.props.pages - 1);
   }
 
   applyPage(e) {
     if (e) {
-      e.preventDefault()
+      e.preventDefault();
     }
-    const page = this.state.page
-    this.changePage(page === '' ? this.props.page : page)
+    const page = this.state.page;
+    this.changePage(page === "" ? this.props.page : page);
   }
 
   onKeyPress(e) {
     if (e.which === 13 || e.keyCode === 13) {
-      this.applyPage()
+      this.applyPage();
     }
   }
 
   onPageChange(e) {
     const val = e.target.value;
-    const page = val - 1
+    const page = val - 1;
 
-    if (val === '') {
-      return this.setState({ page: val })
+    if (val === "") {
+      return this.setState({ page: val });
     }
 
-    this.setState({ page: this.getSafePage(page) })
+    this.setState({ page: this.getSafePage(page) });
   }
 
-  changePage (page) {
+  changePage(page) {
     page = this.getSafePage(page);
     this.setState({ page });
     if (this.props.page !== page) {
@@ -78,49 +79,65 @@ class Pagination extends Component {
   }
 
   onPageSizeChange(e) {
-    const {onPageSizeChange}=this.props;
+    const { onPageSizeChange } = this.props;
     const value = parseInt(e.target.value, 10);
     // we need to reset page in the state in order to get correct new page, however we need
     // to do this for all pages except first one
     if (this.state.page !== 0) {
-      this.setState({page: -1});
+      this.setState({ page: -1 });
     }
     onPageSizeChange(value);
   }
 
   render() {
-    const {extraData, page, canPrevious, canNext, defaultPage, defaultPageSize, onPageChange, onPageSizeChange, pageSize, pageSizeOptions, pages, rowsSelectorText, rowsText, pageJumpText} = this.props;
+    const {
+      extraData,
+      page,
+      canPrevious,
+      canNext,
+      defaultPage,
+      defaultPageSize,
+      onPageChange,
+      onPageSizeChange,
+      pageSize,
+      pageSizeOptions,
+      pages,
+      rowsSelectorText,
+      rowsText,
+      pageJumpText
+    } = this.props;
 
-    return (<div className="Pagination">
-      <div className="Pagination-left">{extraData}</div>
-      <div className="Pagination-center">
-        <button
-          type="button"
-          className="Pagination-button Pagination-buttonPrev"
-          onClick={() => this.changePage(page - 1)}
-          disabled={!canPrevious}
-        >
-            <img src="/images/arrow-left.png" alt=""/>
+    return (
+      <div className="Pagination">
+        <div className="Pagination-left">{extraData}</div>
+        <div className="Pagination-center">
+          <button
+            type="button"
+            className="Pagination-button Pagination-buttonPrev"
+            onClick={() => this.changePage(page - 1)}
+            disabled={!canPrevious}
+          >
+            <img src="/images/arrow-left.png" alt="" />
             Previous
-        </button>
-        <button
-          type="button"
-          className="Pagination-button Pagination-buttonNext"
-          onClick={() => this.changePage(page + 1)}
-          disabled={!canNext}
-        >
+          </button>
+          <button
+            type="button"
+            className="Pagination-button Pagination-buttonNext"
+            onClick={() => this.changePage(page + 1)}
+            disabled={!canNext}
+          >
             Next
-            <img src="/images/arrow-right.png" alt=""/>
-        </button>
-      </div>
-      <div className="Pagination-right">
-        <div className="Pagination-pages">
+            <img src="/images/arrow-right.png" alt="" />
+          </button>
+        </div>
+        <div className="Pagination-right">
+          <div className="Pagination-pages">
             <span>Page&nbsp;</span>
             <input
               aria-label={pageJumpText}
               type="number"
               className="Pagination-input Pagination-pageSelector"
-              value={this.state.page === '' ? '' : this.state.page + 1}
+              value={this.state.page === "" ? "" : this.state.page + 1}
               onChange={this.onPageChange}
               onBlur={this.applyPage}
               onKeyPress={this.onKeyPress}
@@ -128,18 +145,21 @@ class Pagination extends Component {
               max={pages}
             />
             <span>{` of ${pages}`}</span>
+          </div>
+          <select
+            aria-label={rowsSelectorText}
+            className="Pagination-input Pagination-pageSize"
+            value={pageSize}
+            onChange={this.onPageSizeChange}
+          >
+            {pageSizeOptions.map((option, i) => (
+              <option key={i} value={option}>{`${option} ${rowsText}`}</option>
+            ))}
+          </select>
         </div>
-        <select
-          aria-label={rowsSelectorText}
-          className="Pagination-input Pagination-pageSize"
-          value={pageSize}
-          onChange={this.onPageSizeChange}
-        >
-          {pageSizeOptions.map((option, i) => <option key={i} value={option}>{`${option} ${rowsText}`}</option>)}
-        </select>
       </div>
-    </div>)
+    );
   }
 }
 
-export default Pagination
+export default Pagination;
--- src/main/app/src/components/home/home.js
+++ src/main/app/src/components/home/home.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import Websocket from "react-websocket";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
-import Page from '../Page'
-import Websocket from 'react-websocket';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
-import SymbolDetails from '../symbol-details';
-import '../../Table.css';
-import Pagination from '../Pagination';
+import Page from "../Page";
+import Pagination from "../Pagination";
+import SymbolDetails from "../symbol-details";
+import "../../Table.css";
 
 class Home extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -20,7 +19,7 @@ class Home extends Component {
         this.sendMessage = this.sendMessage.bind(this);
         this.handleData = this.handleData.bind(this);
         this.onOpen = this.onOpen.bind(this);
-        this.once = true
+        this.once = true;
     }
 
     sendMessage(message) {
@@ -28,107 +27,120 @@ class Home extends Component {
     }
 
     onOpen() {
-        setInterval(() => this.sendMessage('LOAD_SYMBOLS'), 1000);
+        setInterval(() => this.sendMessage("LOAD_SYMBOLS"), 1000);
     }
 
     handleData(data) {
         let result = JSON.parse(data);
         for (let i = 0; i < result.symbols.length; i++) {
             let oldPrice;
-            if (typeof this.state.symbols[i] === 'undefined') {
+            if (typeof this.state.symbols[i] === "undefined") {
                 oldPrice = 0;
             } else {
                 oldPrice = this.state.symbols[i].price;
             }
             result.symbols[i]["oldPrice"] = oldPrice;
         }
-        this.setState({symbols: result.symbols});
+        this.setState({ symbols: result.symbols });
     }
 
     render() {
-        const {symbols} = this.state;
+        const { symbols } = this.state;
 
         const columns = [
             {
-                Header: 'Symbol',
-                accessor: 'symbol',
-                Cell: ({value}) => <span className='Table-highlightValue'>{value}</span>,
-            },
-            {
-                Header: 'Name',
-                accessor: 'name'
-            },
-            {
-                Header: 'Price',
-                accessor: 'price',
-                Cell: ({value, columnProps: {className}}) => <span className={`Table-highlightValue Table-price ${className}`}>{(value/100).toLocaleString("en-US", {style:"currency", currency:"USD"})}</span>,
-                getProps: (state, ri, column) => {
-                    if (!ri){
-                        return {};
-                    }
-                    // console.log(ri.row);
-                    const changeUp = ri.row.price > ri.row._original.oldPrice;
-                    const changeDown = ri.row.price < ri.row._original.oldPrice;
-                    const className =
-                    changeUp ? 'Table-changeUp' : (changeDown ? 'Table-changeDown' : '')
+                Header: "Symbol",
+                accessor: "symbol",
+                width: 300,
+                Cell: ({ value }) => (
+                    <span className="Table-highlightValue">{value}</span>
+                    )
+                },
+                {
+                    Header: "Name",
+                    accessor: "name"
+                },
+                {
+                    Header: "Price",
+                    accessor: "price",
+                    width: 300,
+                    Cell: ({ value, columnProps: { className } }) => (
+                        <span className={`Table-highlightValue Table-price ${className}`}>
+                        {(value / 100).toLocaleString("en-US", {
+                            style: "currency",
+                            currency: "USD"
+                        })}
+                        </span>
+                        ),
+                        getProps: (state, ri, column) => {
+                            if (!ri) {
+                                return {};
+                            }
+                            // console.log(ri.row);
+                            const changeUp = ri.row.price > ri.row._original.oldPrice;
+                            const changeDown = ri.row.price < ri.row._original.oldPrice;
+                            const className = changeUp
+                            ? "Table-changeUp"
+                            : changeDown
+                            ? "Table-changeDown"
+                            : "";
 
-                    return {
-                        className,
-                    };
-                }
-            },
-            {
-                Header: 'Volume',
-                accessor: 'volume'
-            },
-        ];
-        return <Page header="Trade Monitor Dashboard">
-        <ReactTable
-        className="Table-main"
-        data={symbols}
-        columns={columns}
-        defaultPageSize={25}
-        expanded={this.state.expanded}
-        onExpandedChange={expanded => this.setState({expanded})}
-        // PaginationComponent={ReactTablePagination}
-        PaginationComponent={Pagination}
-        getTrProps={(state, rowInfo) => ({
-            className: rowInfo && state.expanded[rowInfo.index] ? 'Table-expanded' : ''}
-        )}
-        getTdProps={(state, rowInfo, column, instance) => {
-            return {
-                onClick: (e, handleOriginal) => {
-                    const {index} = rowInfo;
-                    this.setState(prevState => ({
-                        expanded: {
-                            ...prevState.expanded,
-                            [index]: !prevState.expanded[index],
+                            return {
+                                className
+                            };
                         }
-                    }))
-                    // IMPORTANT! React-Table uses onClick internally to trigger
-                    // events like expanding SubComponents and pivots.
-                    // By default a custom 'onClick' handler will override this functionality.
-                    // If you want to fire the original onClick handler, call the
-                    // 'handleOriginal' function.
-                    // if (handleOriginal) {
-                    //   handleOriginal()
-                    // }
+                    },
+                    {
+                        Header: "Volume",
+                        accessor: "volume"
+                    }
+                ];
+                return (
+                    <Page header="Trade Monitor Dashboard">
+                    <ReactTable
+                    className="Table-main"
+                    data={symbols}
+                    columns={columns}
+                    defaultPageSize={25}
+                    expanded={this.state.expanded}
+                    onExpandedChange={expanded => this.setState({ expanded })}
+                    PaginationComponent={Pagination}
+                    getTrProps={(state, rowInfo) => ({
+                        className:
+                        rowInfo && state.expanded[rowInfo.index] ? "Table-expanded" : ""
+                    })}
+                    getTdProps={(state, rowInfo, column, instance) => {
+                        return {
+                            onClick: (e, handleOriginal) => {
+                                const { index } = rowInfo;
+                                this.setState(prevState => ({
+                                    expanded: {
+                                        ...prevState.expanded,
+                                        [index]: !prevState.expanded[index]
+                                    }
+                                }));
+                            }
+                        };
+                    }}
+                    //   NoDataComponent={}
+                    SubComponent={original => (
+                        <SymbolDetails symbol={original.row.symbol} />
+                        )}
+                        />
+
+                        <Websocket
+                        url="ws://localhost:9000/trades"
+                        onOpen={this.onOpen}
+                        onMessage={this.handleData}
+                        reconnect={true}
+                        debug={true}
+                        ref={Websocket => {
+                            this.refWebSocket = Websocket;
+                        }}
+                        />
+                        </Page>
+                        );
+                    }
                 }
-            }
-        }}
-        //   NoDataComponent={}
-        SubComponent={original => <SymbolDetails
-            symbol={original.row.symbol}/>}
-            />
 
-            <Websocket url='ws://localhost:9000/trades' onOpen={this.onOpen}
-            onMessage={this.handleData}
-            reconnect={true} debug={true}
-            ref={Websocket => {
-                this.refWebSocket = Websocket;
-            }}/>
-            </Page>;
-        }
-    }
-
-    export default Home
+                export default Home;
